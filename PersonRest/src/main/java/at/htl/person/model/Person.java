baa97958bc2eb920d37c3ac6c67cc43994b34e27
woegerbauerjakob <jaki.woe@gmail.com>
2019-11-07 16:47:40 +0100

    reworked files

--- PersonRest/src/main/java/at/htl/person/model/Person.java
+++ PersonRest/src/main/java/at/htl/person/model/Person.java
@@ -1,33 +1,53 @@
 package at.htl.person.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Entity
 public class Person{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
     private String name;
-    private LocalDate dob;
+    private LocalDate dob = LocalDate.now();
 
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
+    public Person(){}
+    public Person(Long id, String name, LocalDate dob)
+    {
         this.id = id;
+        this.dob = dob;
+        this.name = name;
+    }
+    public Person(String name){
+        this.name = name;
     }
 
+    ///region Getter and Setter
     public Long getId(){
         return id;
     }
     public void setId(Long id){
         this.id = id;
     }
-    public Long getId(){
-        return id;
+    public String getname(){
+        return name;
     }
-    public void setId(Long id){
-        this.id = id;
+    public void setname(String name){
+        this.name = name;
+    }
+    public LocalDate getdob(){
+        return dob;
+    }
+    public void setdob(LocalDate dob){
+        this.dob = dob;
+    }
+    ///end region Getter and Setter
+
+    public String toString()
+    {
+        return id.toString() + name + dob.format(DateTimeFormatter.ofPattern("DD.MM.YYYY"));
     }
 }
